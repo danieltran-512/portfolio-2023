2a1094ffa1cbe998718524a062707035af82716e
Daniel <xkw1927@autuni.ac.nz>
2023-03-26 12:10:11 +1300

    Update LandingPage.tsx

--- components/pages/LandingPage.tsx
+++ components/pages/LandingPage.tsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-import styles from "../../styles/LandingPage.module.css";
-import SmoothScroll from "../Animations/SmoothScroll";
-import Button from "../Button";
 import Cursor from "../Cursor";
 import Logo from "../Logo";
 import AboutMe from "./landingPageComponents/AboutMeSection";
